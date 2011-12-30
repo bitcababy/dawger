4ad23c6502f918370c323e621f34f5abcdc4c962
Meredith Lesly <bitcababy+github@gmail.com>
2011-12-30 13:58:45 -0500

    Added tests for Dawger.from_array and Dawger.from_file

--- spec/dawger/core_spec.rb
+++ spec/dawger/core_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
-describe Dawger do
-	describe "::from_array" do
+describe Dawger::Core do
+	describe "::from_array(array)" do
 		it "takes an array of words and turns them into a dawg" do
 			words = %W(city cities pity pities)
-			dawg = Dawg.from_array
+			dawg = Dawger::Core.from_array words
 		end
+	end
+	
+	describe "::from_file(filehandle)" do
+		before :each do 
+			@text = <<EOF
+city
+cities
+pity
+pities
+EOF
+		end
+			
+		it "receives a file and creates a dawg from the list of words" do
+			# file = FakeFS::File.new
+			# file.content = @text
+			file = StringIO.open @text
+			dawg = Dawger::Core.from_file(file)
+			dawg.should_not be_nil
+			dawg.should be_kind_of Dawg
+			dawg.find('city').should be_true
+		end
+			
 	end
 
 end
