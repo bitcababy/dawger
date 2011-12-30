457796bdffb11438cc358b5e242d5335c2c10d04
Meredith Lesly <bitcababy+github@gmail.com>
2011-12-30 13:56:33 -0500

    Requires the necessary files (I hope)

--- lib/dawger.rb
+++ lib/dawger.rb
@@ -1,16 +1,4 @@
 require 'dawger/version'
-require 'dawg/base'
-require 'dawg/node'
 require 'dawger/core'
-
-module Dawger
-	class << self
-		def from_array(words = [])
-			dawg = Dawg::Base.new
-			for word in words do
-				dawg.add word
-			end
-			return dawg
-		end
-	end
-end
+require 'dawg'
+require 'dawg/node'
