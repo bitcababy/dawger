ec191d1defa8b9d3e6a830df45d582e71456e724
Meredith Lesly <bitcababy+github@gmail.com>
2011-12-30 13:57:05 -0500

    Refactored code to dawg.rb

--- lib/dawger/core.rb
+++ lib/dawger/core.rb
@@ -1,12 +1,16 @@
-require 'dawg/base'
+require 'dawg'
 
 module Dawger
 	class Core
 		def self.from_array(words = [])
-			dawg = Dawg::Base.new
-			for word in words do
-				dawg.add word
-			end
+			dawg = Dawg.new
+			dawg.add_array(words)
+			return dawg
+		end
+	
+		def self.from_file(file)
+			dawg = Dawg.new
+			dawg.from_file(file)
 			return dawg
 		end
 	end
