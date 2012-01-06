22891b38107c65627013475bf6b912fd1e64f254
Meredith Lesly <bitcababy+github@gmail.com>
2012-01-06 14:16:43 -0500

    Added a bunch of test cases (all in the green)

--- spec/dawg/node_spec.rb
+++ spec/dawg/node_spec.rb
@@ -1,26 +1,9 @@
 require 'spec_helper'
 
-module DawgNodeHelper
-	def add_kid(node, ch)
-		node.letters[ch] = Dawg::Node.new
-	end
-	
-	def from_str(str="")
-		node = root = Dawg::Node.new
-		str.each_char do |ch|
-			node = node.letters[ch] = Dawg::Node.new
-		end
-		return root
-	end
-	
-	def check_node_in_arr(node, arr)
-		arr[node.max_depth].index(node)
-	end
-	
-end
-
 describe Dawg::Node do
-	include DawgNodeHelper
+	before :each do
+		@root = Dawg::Node.new
+	end
 
 	describe ':==' do
 		it "returns true if its components all match" do
@@ -28,96 +11,153 @@ describe Dawg::Node do
 			node1.letters = {'y' => {}}
 			node1.eow = true
 			node1.max_depth = 0
-		
+	
 			node2 = Dawg::Node.new
 			node2.letters = {'y' => {}}
 			node2.eow = true
 			node2.max_depth = 0
-		
+	
 			node1.should == node2
 		end
 	end
+
 	describe ':===' do
 		it "returns true if its components all match" do
 			node1 = Dawg::Node.new
 			node1.letters = {'y' => {}}
 			node1.eow = true
 			node1.max_depth = 0
-		
+	
 			node2 = Dawg::Node.new
 			node2.letters = {'y' => {}}
 			node2.eow = true
 			node2.max_depth = 0
-		
+	
 			node1.should === node2
 		end
 	end
-	
-	describe'#empty?' do
-		it "returns true if letters are empty" do
-			node = Dawg::Node.new
-			node.empty?.should be_true
+
+	describe '#add_char_array' do
+		it "should set @eow if the array is empty" do
+			@root.add_char_array([])
+			@root.eow.should be_true
 		end
-		it "returns false if letters aren't empty" do
-			node = Dawg::Node.new
-			add_kid(node, 'p')
-			node.empty?.should be_false
+		it "adds a node for the first letter" do
+			@root.add_char_array %w(d o)
+			@root['d'].should_not be_nil
 		end
 		
+		it "calls itself recursively for the rest of the array" do
+			@root.add_char_array %w(d o)
+			child = @root['d'].letters['o']
+			child.should_not be_nil
+			child.eow.should be_true
+		end
+	end
+
+	describe '#add_str(str)' do
+		it "adds a string of letters to itself, creating child nodes as necessary" do
+			@root.add_str('do')
+			@root['d'].should_not be_nil
+			child = @root['d']['o']
+			child.should_not be_nil
+			child.eow.should be_true
+		end
+	end
+
+	describe'#empty?' do
+		it "returns true if letters are empty" do
+			@root.should be_empty
+		end
+		it "returns false if letters aren't empty" do
+			@root.add_str('p')
+			@root.should_not be_empty
+		end
 	end
 
 	describe '#add_depths' do
 		it "sets the @max_depth property" do
-			root = from_str('city')
-			root.add_depths
-			root.max_depth.should eq(3)
-			root.letters['c'].max_depth.should eq(2)
+			word = 'cat'
+			@root.add_str(word)
+			@root.add_depths
+
+			md = word.length - 1
+			node = @root
+			word.each_char do |ch|
+				node.max_depth.should eq(md)
+				node = node.letters[ch]
+				md -= 1
+			end
 		end
 	end
 
 	describe '#add_to_depth_array' do
 		before :each do
-			@node_array = {}
+			@node_array = []
 			0.upto(10) {|i| @node_array[i] = []}
 		end
 
 		it "adds itself to array[0] if it's a leaf" do
-			root = from_str("a")
-			root.add_depths
-			root.add_to_depth_array(@node_array)
-			@node_array[0].index(root).should_not be_nil
+			@root.add_str("a")
+			@root.add_depths
+			@root.add_to_depth_array(@node_array)
+			@node_array[0].index(@root).should_not be_nil
 		end
-		
+	
 		it "adds itself and its children if it's not a leaf" do
 			word = 'cat'
-			root = from_str(word)
-			root.add_depths
-			root.add_to_depth_array(@node_array)
-			
-			check_node_in_arr(root, @node_array).should_not be_nil
-			node = root['c']
-			check_node_in_arr(node, @node_array).should_not be_nil
-			node = node['a']
-			check_node_in_arr(node, @node_array).should_not be_nil
-			node = node['t']
-			node.empty?.should be_true
+			@root.add_str(word)
+			@root.add_depths
+			@root.add_to_depth_array(@node_array)
+		
+			node = @root
+		
+			word.each_char do |ch|
+				@node_array[node.max_depth].index(node).should_not be_nil, "Expected #{node} to be in @node_array[#{node.max_depth}]"
+				node = node[ch]
+			end
+
 		end
 	end
 
-	
-end # Dawg::Node
-	
-	
-	# 
-	# def merge_nodes(candidates)
-	# 	@letters.each do |k, v|
-	# 		i = candidates.index v
-	# 		if i then
-	# 			@letters[k] = candidates[i]
-	# 		end
-	# 	end
-	# end
+	describe '#dawgify' do
+		it "sets @max_depth" do
+			@root.add_str('cat')
+			@root.dawgify
+			@root.max_depth.should eq(2)
+		end
+		it "merges nodes" do
+			@root.add_str('city')
+			@root.add_str('pity')
+			@root.dawgify
+			child1 = @root['c']
+			child2 = @root['p']
+			child1['i'].should equal(child2['i'])
+		end
+	end
 
-	
+	describe '#include?' do
+		before :each do 
+			@root.add_str("cart")
+			@root.dawgify
+		end
+		it "returns true if it contains the supplied string" do
+			@root.include?('cart').should be_true
+		end
+			
+		it "returns false if it doesn't contain the supplied string" do
+			@root.include?('dog').should be_false
+			@root.include?('car').should be_false
+		end
+	end
 
+	describe '#add_from_array' do
+		it "adds all the words contained in the array" do
+			words = %W(city cities pity pities)
+			@root.add_from_array words
+			@root.dawgify
+			words.each {|word| @root.include?(word).should be_true}
+		end
+	end
 
+end
